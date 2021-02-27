5588e72c1fe174268d1f061472afedf730405e02
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-27 14:45:21 -0600

    Update devcontainer

---
+++ helloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Adding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
