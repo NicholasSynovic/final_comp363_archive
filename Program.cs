213f1456986f199e159a76b007ee709643d0604e
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-03-02 00:04:58 +0000

    Add files from dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emperor_Card_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
